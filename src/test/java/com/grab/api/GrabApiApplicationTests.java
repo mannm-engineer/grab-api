5de6ce18be565c4738573e7a418ae33a86420225
Man, Nguyen Minh <mannm.engineer@gmail.com>
2025-09-28 14:28:23 +0700

    feat: generate project from spring initalizr

---
+++ src/test/java/com/grab/api/GrabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grab.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
